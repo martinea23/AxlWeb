b7640d1d388651b92c8fba0709b856bdab62f490
martinea23 <martinea23@up.edu>
2023-10-05 13:51:28 -0700

    nav bar formatting for diff sizes

---
+++ Scripts/menuHide.js
@@ -0,0 +1,15 @@
+var menu = document.getElementsByClassName("menuContainer")[0];
+var lastScrollTop = 0
+window.addEventListener("scroll", function(){
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    
+    if (scrollTop > lastScrollTop) {
+        // Scrolling down, hide the menuContainer
+        menu.classList.add("hide");
+    } else {
+        // Scrolling up, show the menuContainer
+        menu.classList.remove("hide");
+    }
+    
+    lastScrollTop = scrollTop;
+})
