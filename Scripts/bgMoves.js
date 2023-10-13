d9deff0dc6d9ae0c8999955669461f506e0edc0d
martinea23 <martinea23@up.edu>
2023-10-13 16:35:42 -0700

    i think this might fix it

--- Scripts/bgMoves.js
+++ Scripts/bgMoves.js
@@ -1,4 +1,4 @@
 const bg = document.getElementById('header');
 window.addEventListener('scroll', function () {
-    bg.style.backgroundPositionY = (0 - window.scrollY/2) + 'px';
+    bg.style.backgroundPositionY = (-500 - window.scrollY/2) + 'px';
 })
